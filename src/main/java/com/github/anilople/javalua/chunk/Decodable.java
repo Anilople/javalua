633e7b2d726dfa2859202441b76debd5e499f11e
wxq <15523186+Anilople@users.noreply.github.com>
2022-01-24 22:33:35 +0800

    refactor: dump and undump

--- src/main/java/com/github/anilople/javalua/chunk/Decodable.java
+++ src/main/java/com/github/anilople/javalua/chunk/Decodable.java
@@ -3,7 +3,10 @@ package com.github.anilople.javalua.chunk;
 import java.io.IOException;
 
 /**
+ * 解码，和undump对应
+ *
  * @author wxq
+ * @see <a href="https://github.com/lua/lua/blob/5d708c3f9cae12820e415d4f89c9eacbe2ab964b/lundump.c">lundump.c</a>
  */
 interface Decodable {
 
--- src/main/java/com/github/anilople/javalua/chunk/Encodable.java
+++ src/main/java/com/github/anilople/javalua/chunk/Encodable.java
@@ -1,30 +1,21 @@
 package com.github.anilople.javalua.chunk;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-
 /**
+ * 编码，和 dump 对应
+ *
  * @author wxq
+ * @see <a href="https://github.com/lua/lua/blob/5d708c3f9cae12820e415d4f89c9eacbe2ab964b/ldump.c">ldump.c</a>
  */
 public interface Encodable {
 
-  /**
-   * i.e dump
-   *
-   * @see <a href="https://github.com/lua/lua/blob/5d708c3f9cae12820e415d4f89c9eacbe2ab964b/ldump.c">ldump.c</a>
-   */
   byte[] encode();
 
   static byte[] encode(Encodable[] encodables) {
-    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+    EncodeOutputStream outputStream = new EncodeOutputStream();
     for (Encodable encodable : encodables) {
       byte[] bytes = encodable.encode();
-      try {
-        byteArrayOutputStream.write(bytes);
-      } catch (IOException e) {
-        throw new IllegalStateException("cannot happen", e);
-      }
+      outputStream.writeBytes(bytes);
     }
-    return byteArrayOutputStream.toByteArray();
+    return outputStream.toByteArray();
   }
 }
