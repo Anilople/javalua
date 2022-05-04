a1544c34c5e7b26571ab195d2556a66131dd9de2
wxq <15523186+Anilople@users.noreply.github.com>
2022-05-05 00:18:35 +0800

    test: fix import

--- javalua-vm/javalua-vm-test/src/test/java/ch07/Page138Test.java
+++ javalua-vm/javalua-vm-test/src/test/java/ch07/Page138Test.java
@@ -4,9 +4,13 @@ import com.github.anilople.javalua.api.LuaVM;
 import com.github.anilople.javalua.chunk.BinaryChunk;
 import com.github.anilople.javalua.instruction.Instruction;
 import com.github.anilople.javalua.instruction.Opcode;
+import com.github.anilople.javalua.state.LuaInteger;
+import com.github.anilople.javalua.state.LuaString;
 import constant.ResourceContentConstants.ch07;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @author wxq
@@ -24,7 +28,7 @@ class Page138Test {
     luaVM.setTop(nRegisters);
 
     // 指令数量
-    Assertions.assertEquals(14, mainFunc.getCode().getCode().length);
+    assertEquals(14, mainFunc.getCode().getCode().length);
     for (Instruction instruction = luaVM.fetch();
         !instruction.getOpcode().equals(Opcode.RETURN);
         instruction = luaVM.fetch()) {
@@ -32,10 +36,10 @@ class Page138Test {
       LuaVM.applyPrint(instruction, luaVM);
     }
     // pc:13	stack:[Table:3 {"foo" = "Bar",1 = "a",2 = "B",3 = "c"}]["cBaBar3"]["B"]["a"]["Bar"][3]
-    Assertions.assertEquals(LuaString.newLuaString("cBaBar3"), luaVM.toLuaString(2));
-    Assertions.assertEquals(LuaString.newLuaString("B"), luaVM.toLuaString(3));
-    Assertions.assertEquals(LuaString.newLuaString("a"), luaVM.toLuaString(4));
-    Assertions.assertEquals(LuaString.newLuaString("Bar"), luaVM.toLuaString(5));
-    Assertions.assertEquals(LuaInteger.newLuaInteger(3), luaVM.toLuaInteger(6));
+    assertEquals(LuaString.newLuaString("cBaBar3"), luaVM.toLuaString(2));
+    assertEquals(LuaString.newLuaString("B"), luaVM.toLuaString(3));
+    assertEquals(LuaString.newLuaString("a"), luaVM.toLuaString(4));
+    assertEquals(LuaString.newLuaString("Bar"), luaVM.toLuaString(5));
+    assertEquals(LuaInteger.newLuaInteger(3), luaVM.toLuaInteger(6));
   }
 }
